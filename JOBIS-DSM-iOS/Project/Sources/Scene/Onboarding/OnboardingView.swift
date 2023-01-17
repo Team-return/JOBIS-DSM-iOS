8094f389ad41ee79d0facafbe33e0c5695f36e55
hsj._.06 <gtw030488@gmail.com>
2023-01-17 18:23:25 +0900

    🔨 :: 파일 Setting

---
+++ JOBIS-DSM-iOS/Project/Sources/Scene/Onboarding/OnboardingView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct OnboardingView: View {
+
+    @State var isSignupViewPresented: Bool = false
+    @State var isLoginViewPresented: Bool = false
+
+    var body: some View {
+        VStack {
+//            OnboardingImageViewer()
+//            FillButton(
+//                text: "바로 시작하기",
+//                action: { self.isSignupViewPresented = true },
+//                type: .rounded
+//            )
+//            Button(action: {
+//                self.isLoginViewPresented = true
+//            }, label: {
+//                HStack {
+//                    Text("이미 계정이 있으신가요?")
+//                        .sdText(type: .body4)
+//                    Text("로그인하기")
+//                        .sdText(type: .body3)
+//                }
+//            })
+//            Spacer(minLength: 40)
+        }
+        .ignoresSafeArea(edges: .top)
+        .fullScreenCover(isPresented: self.$isSignupViewPresented) {
+            Text("회원가입 화면")
+        }
+        .fullScreenCover(isPresented: self.$isLoginViewPresented) {
+            Text("로그인 화면")
+        }
+    }
+}
