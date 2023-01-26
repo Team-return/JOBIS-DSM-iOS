514d19e5ea97befd399455bf09a745da71e2daa9
hsj._.06 <gtw030488@gmail.com>
2023-01-26 21:39:48 +0900

    Tuist 적용 (실패)

---
+++ JOBIS-iOS/Targets/JOBISIOSKit/Tests/JOBISIOSKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class JOBISIOSKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("JOBISIOSKit", "JOBISIOSKit")
+    }
+}
---
+++ JOBIS-iOS/Targets/JOBISIOSUI/Tests/JOBISIOSUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class JOBISIOSUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("JOBISIOSUI", "JOBISIOSUI")
+    }
+}
