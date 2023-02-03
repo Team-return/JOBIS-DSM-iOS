5c271ee5cd4aa277ff51409bc4c5d3fdd1f3c721
hsj._.06 <gtw030488@gmail.com>
2023-02-03 14:39:13 +0900

    🧩 :: 정정 -> SolidButton 구현완료가 아니라 SolidButton.Default 완성

--- Projects/UsertInterfaces/DesignSystem/Sources/Button/SolidBtn/SolidBtn.swift
+++ Projects/UsertInterfaces/DesignSystem/Sources/Button/SolidBtn/SolidBtn.swift
@@ -4,36 +4,37 @@ public struct SolidBtn: View {
     var text: String
     var style: SolidBtnStyle.Style
     var action: () -> Void
+    var size: SolidBtnStyle.Size
 
     public init(
         text: String = "",
         style: SolidBtnStyle.Style = .defaultStyle,
         color: Color = .Main.lightBlue,
-        action: @escaping () -> Void = {}
+        action: @escaping () -> Void = {},
+        size: SolidBtnStyle.Size = .medium
     ) {
         self.text = text
         self.style = style
         self.action = action
+        self.size = size
     }
 
     public var body: some View {
         Button(action: action) {
             Text(text)
         }
-        .buttonStyle(SolidBtnStyle(style: style))
+        .buttonStyle(SolidBtnStyle(style: style, size: size))
     }
 }
 
 struct SolidDefaultBtn_Previews: PreviewProvider {
     static var previews: some View {
-//        VStack(spacing: 20) {
-//            SolidDefaultBtn(text: "Contained")
-//            SolidDefaultBtn(text: "Outlined", style: .outlined)
-//            SolidDefaultBtn(text: "Text", style: .text)
-//            SolidDefaultBtn(text: "Underline", style: .underline)
-//        }
-//        .padding()
-//        .background(Color.Sub.gray90)
-        SolidBtn(text: "버튼")
+        VStack(spacing: 20) {
+            SolidBtn(text: "버튼", size: .small)
+            SolidBtn(text: "버튼", size: .medium)
+            SolidBtn(text: "버튼", size: .large)
+        }
+        .padding(100)
+        .background(Color.Sub.gray50)
     }
 }
--- Projects/UsertInterfaces/DesignSystem/Sources/Button/SolidBtn/SolidDefaultBtn.swift
+++ Projects/UsertInterfaces/DesignSystem/Sources/Button/SolidBtn/SolidDefaultBtn.swift
@@ -8,29 +8,82 @@ public struct SolidBtnStyle: ButtonStyle {
         case icon1
         case icon2
     }
+    public enum Size {
+        case small
+        case medium
+        case large
+    }
 
     var style: Style
+    var size: Size
 
     public func makeBody(configuration: Configuration) -> some View {
         switch style {
         case .defaultStyle:
-            return AnyView(DefaultButton(configuration: configuration))
+            return AnyView(DefaultButton(configuration: configuration, size: size))
         case .lIcon:
-            return AnyView(LIconButton(configuration: configuration))
+            return AnyView(LIconButton(configuration: configuration, size: size))
         case .rIcon:
-            return AnyView(RIconButton(configuration: configuration))
+            return AnyView(RIconButton(configuration: configuration, size: size))
         case .icon1:
             return AnyView(Icon1Button(configuration: configuration))
         case .icon2:
-            return AnyView(Icon2Button(configuration: configuration))
+            return AnyView(Icon2Button(configuration: configuration, size: size))
         }
+    }
+}
+
+private struct ButtonStruct {
+    let minWidth: CGFloat
+    let minHeight: CGFloat
+    let hPadding: CGFloat
+    let cornerRadius: CGFloat
+    let font: JOBISFontStyle
+    public init(
+        minWidth: CGFloat,
+        minHeight: CGFloat,
+        hPadding: CGFloat,
+        cornerRadius: CGFloat,
+        font: JOBISFontStyle
+    ) {
+        self.minWidth = minWidth
+        self.minHeight = minHeight
+        self.hPadding = hPadding
+        self.cornerRadius = cornerRadius
+        self.font = font
+    }
+}
+
+private func buttonSize(size: SolidBtnStyle.Size) -> ButtonStruct {
+    switch size {
+    case .small:
+        return ButtonStruct(
+            minWidth: 70,
+            minHeight: 30,
+            hPadding: 20,
+            cornerRadius: 15,
+            font: .body(.body2))
+    case .medium:
+        return ButtonStruct(
+            minWidth: 130,
+            minHeight: 44,
+            hPadding: 15,
+            cornerRadius: 8,
+            font: .heading(.heading6))
+    case .large:
+        return ButtonStruct(
+            minWidth: 320,
+            minHeight: 50,
+            hPadding: 10,
+            cornerRadius: 12,
+            font: .heading(.heading6))
     }
 }
 
 // MARK: - Usage
 extension Button {
-    func solidBtnStyle(_ style: SolidBtnStyle.Style, color: Color) -> some View {
-        self.buttonStyle(SolidBtnStyle(style: style))
+    func solidBtnStyle(_ style: SolidBtnStyle.Style, size: SolidBtnStyle.Size) -> some View {
+        self.buttonStyle(SolidBtnStyle(style: style, size: size))
     }
 }
 
@@ -38,15 +91,17 @@ extension Button {
 extension SolidBtnStyle {
     struct DefaultButton: View {
         let configuration: ButtonStyle.Configuration
+        let size: SolidBtnStyle.Size
         @Environment(\.isEnabled) private var isEnabled: Bool
         var body: some View {
             configuration.label
-                .JOBISFont(.body(.body2), color: .white)
-                .padding(.horizontal, 20)
-                .padding(.vertical, 5)
+                .JOBISFont(buttonSize(size: size).font, color: .white)
+                .padding(.horizontal, buttonSize(size: size).hPadding)
+                .frame(minWidth: buttonSize(size: size).minWidth, minHeight: buttonSize(size: size).minHeight)
                 .background(isEnabled ? configuration.isPressed ? Color.Main.darkBlue : Color.Main.lightBlue
                             : Color.Sub.gray50)
-                .cornerRadius(15)
+                .background(Color.Main.lightBlue)
+                .cornerRadius(buttonSize(size: size).cornerRadius)
         }
     }
 }
@@ -55,6 +110,7 @@ extension SolidBtnStyle {
 extension SolidBtnStyle {
     struct LIconButton: View {
         let configuration: ButtonStyle.Configuration
+        let size: SolidBtnStyle.Size
         @Environment(\.isEnabled) private var isEnabled: Bool
         var body: some View {
             configuration.label
@@ -77,6 +133,7 @@ extension SolidBtnStyle {
 extension SolidBtnStyle {
     struct RIconButton: View {
         let configuration: ButtonStyle.Configuration
+        let size: SolidBtnStyle.Size
         @Environment(\.isEnabled) private var isEnabled: Bool
         var body: some View {
             configuration.label
@@ -127,6 +184,7 @@ extension SolidBtnStyle {
 extension SolidBtnStyle {
     struct Icon2Button: View {
         let configuration: ButtonStyle.Configuration
+        let size: SolidBtnStyle.Size
         @Environment(\.isEnabled) private var isEnabled: Bool
         var body: some View {
             configuration.label
