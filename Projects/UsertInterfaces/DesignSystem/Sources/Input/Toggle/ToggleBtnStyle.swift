7b38fe668e08153cba99b1e8203daa225d238193
hsj._.06 <gtw030488@gmail.com>
2023-02-11 02:08:33 +0900

    📝 :: Swift Lint error 수정

--- Projects/UsertInterfaces/DesignSystem/Sources/Input/Toggle/ToggleBtnStyle.swift
+++ Projects/UsertInterfaces/DesignSystem/Sources/Input/Toggle/ToggleBtnStyle.swift
@@ -2,7 +2,6 @@ import SwiftUI
 
 public struct ToggleBtnStyle: ToggleStyle {
     @Environment(\.isEnabled) var isEnabled: Bool
-      
     public func makeBody(configuration: Configuration) -> some View {
         HStack {
           configuration.label
@@ -11,7 +10,7 @@ public struct ToggleBtnStyle: ToggleStyle {
               .frame(width: 48, height: 24)
               .foregroundColor(isEnabled ? configuration.isOn ? .Icon.input : .Sub.gray50 :  .Sub.gray40)
             Circle()
-              .frame(width: 18,height: 18)
+              .frame(width: 18, height: 18)
               .padding(3)
               .foregroundColor(isEnabled ? .Sub.gray10 : .Sub.gray50)
               .onTapGesture {
