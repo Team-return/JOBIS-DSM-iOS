26f6e627fd2cc85a60ddafa27784bb96a222afc2
hsj._.06 <gtw030488@gmail.com>
2023-02-10 21:58:34 +0900

    🧩 :: JOBISImage

---
+++ Projects/UsertInterfaces/DesignSystem/Sources/Image/JOBISImage.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+public extension Image {
+    struct CheckBox { }
+}
+
+public extension Image.CheckBox {
+    static let checkMark: Image = DesignSystemAsset.Icons.check.suiImage
+}
