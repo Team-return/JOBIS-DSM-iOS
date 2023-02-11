aa14ec431cb8ae225f835b2b3c256aaf327c9bc4
hsj._.06 <gtw030488@gmail.com>
2023-02-11 15:28:17 +0900

    ⚒️ :: JOBISDropdown

---
+++ Projects/UsertInterfaces/DesignSystem/Sources/Input/DropDown/JOBISDropDown.swift
@@ -0,0 +1,21 @@
+//
+//  DropDown.swift
+//  DesignSystem
+//
+//  Created by 홍승재 on 2023/02/11.
+//  Copyright © 2023 team.return. All rights reserved.
+//
+
+import SwiftUI
+
+struct JOBISDropDown: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JOBISDropDown_Previews: PreviewProvider {
+    static var previews: some View {
+        JOBISDropDown()
+    }
+}
