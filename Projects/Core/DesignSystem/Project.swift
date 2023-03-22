e3e37ae5b3b91417ed33893b399f1dcf650a56dd
hsj._.06 <gtw030488@gmail.com>
2023-03-22 11:50:40 +0900

    setting :: project setting 엎기

---
+++ Projects/Core/DesignSystem/Project.swift
@@ -0,0 +1,10 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.makeModule(
+    name: ModulePaths.Core.DesignSystem.rawValue,
+    product: .framework,
+    targets: [.demo],
+    resources: ["Resources/**"]
+)
