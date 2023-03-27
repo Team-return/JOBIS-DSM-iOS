a435d8ed782ade1c6b5d30e66792f562084b2416
hsj._.06 <gtw030488@gmail.com>
2023-03-28 08:36:47 +0900

    remove :: =

--- Projects/Shared/KeychainModule/Project.swift
+++
@@ -1,9 +0,0 @@
-import ProjectDescription
-import ProjectDescriptionHelpers
-import DependencyPlugin
-
-let project = Project.makeModule(
-    name: ModulePaths.Shared.KeychainModule.rawValue,
-    product: .framework,
-    targets: []
-)
