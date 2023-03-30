7022fb2df2bc225fbed2c692c3f8fa71f76dda9d
hsj._.06 <gtw030488@gmail.com>
2023-03-30 20:18:27 +0900

    feat :: JOBISTextFIeld 개발

--- Projects/Core/DesignSystem/Sources/Input/TextField/JOBISTextField.swift
+++ Projects/Core/DesignSystem/Sources/Input/TextField/JOBISTextField.swift
@@ -2,81 +2,199 @@ import SwiftUI
 
 public struct JOBISTextField: View {
     @Environment(\.isEnabled) private var isEnabled: Bool
-    var label: String
+    var placeholder: String
     @Binding var text: String
     var isError: Bool
     var errorMessage: String
-    var bottomMessage: String
-    var topMessage: String
     var onCommit: () -> Void
     @FocusState var isFocused: Bool
+
+    var textfieldType: TextfieldType
+    var textfieldOutlinedType: TextfieldOutlinedType
+    var bottomMessage: String
+    var topMessage: String
+    @State private var isPWSecure = true
+
     public init(
-        _ label: String = "",
+        placeholder: String,
         text: Binding<String>,
-        isError: Bool = false,
-        errorMessage: String = "",
+        isError: Bool,
+        errorMessage: String,
+        textfieldType: TextfieldType = .none,
+        textfieldOutlinedType: TextfieldOutlinedType,
         bottomMessage: String = "",
         topMessage: String = "",
         onCommit: @escaping () -> Void = {}
     ) {
-        self.label = label
+        self.placeholder = placeholder
         _text = text
         self.isError = isError
         self.errorMessage = errorMessage
+        self.textfieldType = textfieldType
+        self.textfieldOutlinedType = textfieldOutlinedType
         self.bottomMessage = bottomMessage
         self.topMessage = topMessage
         self.onCommit = onCommit
     }
+
     public var body: some View {
         ZStack(alignment: .leading) {
-            Text(label)
-                .JOBISFont(.body(.body1), color: .Sub.gray60)
+            Text(placeholder)
+                .JOBISFont(
+                    .body(.body1),
+                    color: isEnabled ? .Sub.gray60 : .Sub.gray50
+                )
                 .opacity(text.isEmpty ? 1 : 0)
-            VStack(alignment: .leading, spacing: 10) {
-                TextField("", text: $text)
-                    .JOBISFont(.body(.body2),
-                               color: isEnabled ?
-                        .Sub.gray90 :
-                            .Sub.gray50)
+            HStack {
+                jobisTextfield()
+                Spacer()
+                textFieldIcon()
             }
         }
         .padding(.vertical, 8)
         .padding(.horizontal, 16)
-        .overlay(
-            RoundedRectangle(cornerRadius: 3)
-                .strokeBorder(
-                    isEnabled ?
-                    isError ?
-                        .State.error :
-                        isFocused ?
-                    Color.Sub.gray60 :
-                            .Sub.gray40 :
-                            .Sub.gray40
-                    ,
-                    lineWidth: 1
-                )
+        .overlay(alignment: .bottom) {
+            if textfieldOutlinedType == .outlined {
+                RoundedRectangle(cornerRadius: 3)
+                    .strokeBorder(
+                        isError && isEnabled ? Color.State.error : isFocused ? .Sub.gray60 : .Sub.gray40,
+                        lineWidth: 1
+                    )
+            } else {
+                Divider()
+                    .frame(height: 2)
+                    .background(isError && isEnabled ? Color.State.error : isFocused ? .Main.lightBlue : .Sub.gray40)
+            }
+        }
+        .background(
+            textfieldOutlinedType == .outlined ?
+            isEnabled ?
+            Color.Sub.gray10 : .Sub.gray30 : .clear
         )
-        .background(isEnabled ? Color.Sub.gray10 : .Sub.gray30)
         .focused($isFocused)
         .onSubmit(onCommit)
         .overlay(alignment: .topLeading) {
             Text(isError ? errorMessage : bottomMessage)
-                .JOBISFont(.etc(.caption),
-                           color: isError ?
-                    .State.error :
-                        .Sub.gray60)
+                .JOBISFont(
+                    .etc(.caption),
+                    color: isError ? .State.error : .Sub.gray60
+                )
                 .opacity(isEnabled ? 1 : 0)
-                .offset(y: 37)
+                .offset(y: 40)
+        }
+        .overlay(alignment: .topLeading) {
+            Text(topMessage)
+                .JOBISFont(
+                    .etc(.caption),
+                    color: isEnabled ? isError ? .State.error : .Sub.gray60 : .Sub.gray50
+                )
+                .offset(y: -18)
         }
         .animation(.easeIn(duration: 0.3), value: isError)
         .animation(.easeIn(duration: 0.3), value: isFocused)
+    }
+
+    @ViewBuilder
+    func jobisTextfield() -> some View {
+        Group {
+            if textfieldType == .password && isPWSecure {
+                SecureField("", text: $text)
+            } else {
+                TextField("", text: $text)
+            }
+        }
+        .JOBISFont(
+            .body(.body2),
+            color: isEnabled ? .Sub.gray90 : .Sub.gray50
+        )
+    }
+
+    @ViewBuilder
+    func textFieldIcon() -> some View {
+        switch textfieldType {
+        case .none:
+            EmptyView()
+        case .password:
+            Button {
+                isPWSecure.toggle()
+            } label: {
+                Image(systemName: isPWSecure ? "eye" : "eye.slash")
+                    .foregroundColor(
+                        isEnabled ? isError ? .State.error : .Sub.gray90 : .Sub.gray50
+                    )
+            }
+        case .search:
+            Button {
+                onCommit()
+            } label: {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(
+                        isEnabled ? isError ? .State.error : .Sub.gray90 : .Sub.gray50
+                    )
+            }
+        }
     }
 }
 
 struct JOBISTextField_Previews: PreviewProvider {
     static var previews: some View {
-        JOBISTextField("Placeholder", text: .constant("asdf`````"), isError: true, errorMessage: "this is error")
+        VStack {
+            JOBISTextField(
+                placeholder: "placeholder",
+                text: .constant(""),
+                isError: false,
+                errorMessage: "errorMessage",
+                textfieldType: .none,
+                textfieldOutlinedType: .bottomlined,
+                bottomMessage: "BottomMessage",
+                topMessage: "TopMessage",
+                onCommit: {
+                    print("asdf")
+                })
             .disabled(false)
             .padding()
+            JOBISTextField(
+                placeholder: "비밀번호를 입력해주세요",
+                text: .constant("gtw030488"),
+                isError: true,
+                errorMessage: "비밀번호를 다시 입력해주세요.",
+                textfieldType: .password,
+                textfieldOutlinedType: .bottomlined,
+                bottomMessage: "@#$%@#$^ <- 이거 다들어가게 넣여용 ^^",
+                topMessage: "비밀번호",
+                onCommit: {
+                }
+            )
+            .disabled(false)
+            .padding()
+            JOBISTextField(
+                placeholder: "placeholder",
+                text: .constant(""),
+                isError: false,
+                errorMessage: "errorMessage",
+                textfieldType: .password,
+                textfieldOutlinedType: .outlined,
+                bottomMessage: "BottomMessage",
+                topMessage: "TopMessage",
+                onCommit: {
+                    print("asdf")
+                })
+            .disabled(false)
+            .padding()
+            JOBISTextField(
+                placeholder: "비밀번호를 입력해주세요",
+                text: .constant("gtw030488"),
+                isError: true,
+                errorMessage: "비밀번호를 다시 입력해주세요.",
+                textfieldType: .password,
+                textfieldOutlinedType: .outlined,
+                bottomMessage: "@#$%@#$^ <- 이거 다들어가게 넣여용 ^^",
+                topMessage: "비밀번호",
+                onCommit: {
+                }
+            )
+            .disabled(false)
+            .padding()
+        }
     }
 }
