5dcd7800720d0153316d6103882bb4df1f6534ef
hsj._.06 <gtw030488@gmail.com>
2023-04-03 10:26:02 +0900

    feat :: RootComponent

--- Projects/Feature/RootFeature/Sources/RootComponent.swift
+++ Projects/Feature/RootFeature/Sources/RootComponent.swift
@@ -1,11 +1,22 @@
-import SwiftUI
-import RootFeatureInterface
+import BaseFeature
 import NeedleFoundation
+import SwiftUI
+import SigninFeature
+import MainTabFeature
+import SplashFeature
 
-public protocol RootDependency: Dependency {}
+public protocol RootDependency: Dependency {
+    var signinComponent: SigninComponent { get }
+    var mainTabComponent: MainTabComponent { get }
+    var splashComponent: SplashComponent { get }
+}
 
-public final class RootComponent: Component<RootDependency>, RootFactory {
+public final class RootComponent: Component<RootDependency> {
     public func makeView() -> some View {
-        RootView(viewModel: .init())
+        RootView(
+            signinComponent: self.dependency.signinComponent,
+            mainTabComponent: self.dependency.mainTabComponent,
+            splashComponent: self.dependency.splashComponent
+        )
     }
 }
