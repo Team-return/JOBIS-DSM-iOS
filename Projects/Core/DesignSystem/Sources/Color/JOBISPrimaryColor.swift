7aa5f92a58c8141acdd3a7a11f7c629a024eaced
hsj._.06 <gtw030488@gmail.com>
2023-04-04 09:09:40 +0900

    feat :: MessageState color

--- Projects/Core/DesignSystem/Sources/Color/JOBISPrimaryColor.swift
+++ Projects/Core/DesignSystem/Sources/Color/JOBISPrimaryColor.swift
@@ -8,4 +8,5 @@ public extension Color.State {
     static let warnig: Color = DesignSystemAsset.Warning.yellow.suiColor
     static let error: Color = DesignSystemAsset.Error.red.suiColor
     static let success: Color = DesignSystemAsset.Success.green.suiColor
+    static let message: Color = DesignSystemAsset.Message.skyBlue.suiColor
 }
