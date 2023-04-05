25b5ec54902cd58070170a7b54e9d413f01c360d
hsj._.06 <gtw030488@gmail.com>
2023-04-05 12:22:02 +0900

    feat :: viewmodel 서버 연동

--- Projects/Feature/SigninFeature/Sources/SigninViewModel.swift
+++ Projects/Feature/SigninFeature/Sources/SigninViewModel.swift
@@ -1,5 +1,30 @@
 import BaseFeature
+import UserDomainInterface
 import Combine
 
 final class SigninViewModel: BaseViewModel {
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var isSuccessSignin = false
+    @Published var authorityEntity: AuthorityEntity?
+
+    private let signinUseCase: SigninUseCase
+
+    public init(signinUseCase: any SigninUseCase) {
+        self.signinUseCase = signinUseCase
+    }
+
+    func signinButtonDidTap() {
+        addCancellable(
+            signinUseCase.execute(
+                req: .init(
+                    accountID: email,
+                    password: password
+                )
+            )
+        ) { [weak self] authority in
+            self?.authorityEntity = authority
+            self?.isSuccessSignin = true
+        }
+    }
 }
