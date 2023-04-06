cd40094d1e65b36967d788bf96b9a5a988d2d433
hsj._.06 <gtw030488@gmail.com>
2023-04-06 22:49:01 +0900

    feat :: SignupFactory

--- Projects/Feature/SignupFeature/Interface/SignupFactory.swift
+++ Projects/Feature/SignupFeature/Interface/SignupFactory.swift
@@ -2,5 +2,8 @@ import SwiftUI
 
 public protocol SignupFactory {
     associatedtype SomeView: View
-    func makeView() -> SomeView
+    func makeView(
+        signupAnimation: Namespace.ID,
+        isPresented: Binding<Bool>
+    ) -> SomeView
 }
