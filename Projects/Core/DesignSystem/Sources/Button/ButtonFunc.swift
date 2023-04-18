e092b2b4e819530a0d82bb49901e10a50daa8f18
hsj._.06 <gtw030488@gmail.com>
2023-04-18 22:15:08 +0900

    fix :: ButtonFunc 에 hPadding 제거

--- Projects/Core/DesignSystem/Sources/Button/ButtonFunc.swift
+++ Projects/Core/DesignSystem/Sources/Button/ButtonFunc.swift
@@ -6,21 +6,18 @@ public func fetchButtonInfo(size: BtnSize) -> ButtonInfo {
         return ButtonInfo(
             minWidth: 70,
             minHeight: 30,
-            hPadding: 20,
             cornerRadius: 15,
             font: .body(.body2))
     case .medium:
         return ButtonInfo(
             minWidth: 130,
             minHeight: 44,
-            hPadding: 15,
             cornerRadius: 8,
             font: .heading(.heading6))
     case .large:
         return ButtonInfo(
             minWidth: 320,
             minHeight: 50,
-            hPadding: 10,
             cornerRadius: 12,
             font: .heading(.heading6))
     }
