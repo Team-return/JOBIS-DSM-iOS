9ed8a4c87e43992200d193a307ef4a9764dc6d26
hsj._.06 <gtw030488@gmail.com>
2023-04-22 20:21:10 +0900

    feat :: SignupComponent

---
+++ Projects/Feature/SignupFeature/Sources/SignupComponent.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+import StudentsDomainInterface
+import SignupFeatureInterface
+import AuthDomainInterface
+import NeedleFoundation
+
+public protocol SignupDependency: Dependency {
+    var signupUseCase: any SignupUseCase { get }
+    var sendAuthCodeUseCase: any SendAuthCodeUseCase { get }
+    var verifyAuthCodeUseCase: any VerifyAuthCodeUseCase { get }
+}
+
+public final class SignupComponent: Component<SignupDependency>, SignupFactory {
+    public func makeView() -> some View {
+        SignupView(
+            viewModel: .init(
+                signupUseCase: dependency.signupUseCase,
+                sendAuthCodeUseCase: dependency.sendAuthCodeUseCase,
+                verifyAuthCodeUseCase: dependency.verifyAuthCodeUseCase
+            )
+        )
+    }
+}
