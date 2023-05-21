142c905e9233f6feb6040f06471aed84aef5f8de
hsj._.06 <gtw030488@gmail.com>
2023-05-21 22:53:19 +0900

    feat :: ApplicationStatusView

--- Projects/Feature/HomeFeature/Sources/Components/ApplicationStatusView.swift
+++ Projects/Feature/HomeFeature/Sources/Components/ApplicationStatusView.swift
@@ -1,21 +1,29 @@
-//
-//  ApplicationStatusView.swift
-//  HomeFeatureInterface
-//
-//  Created by 홍승재 on 2023/05/21.
-//  Copyright © 2023 Team-Return. All rights reserved.
-//
-
 import SwiftUI
+import DesignSystem
 
 struct ApplicationStatusView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+            VStack(alignment: .leading) {
+                Text("지원현황")
+                    .JOBISFont(.etc(.caption), color: .Sub.gray60)
+                    .padding(.bottom, 4)
+                    .padding(.leading, 9)
 
-struct ApplicationStatusView_Previews: PreviewProvider {
-    static var previews: some View {
-        ApplicationStatusView()
-    }
+                HStack {
+                    Spacer()
+
+                    Text("아직 지원한 기업이 없어요")
+                        .JOBISFont(.etc(.caption), color: .Sub.gray50)
+                        .padding(.vertical, 17)
+
+                    Spacer()
+                }
+                .background(Color.Sub.gray10)
+                .cornerRadius(10)
+                .padding(1)
+                .background(Color.Sub.gray40)
+                .cornerRadius(10)
+            }
+            .padding(.horizontal, 30)
+        }
 }
