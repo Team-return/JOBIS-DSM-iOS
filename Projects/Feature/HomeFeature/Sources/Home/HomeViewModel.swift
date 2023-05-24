04b7d8bffad8ee4ab6fdbb2bb8bbc9698650be63
hsj._.06 <gtw030488@gmail.com>
2023-05-24 19:06:31 +0900

    feat :: HomeVM에 학생정보 API 추가

--- Projects/Feature/HomeFeature/Sources/Home/HomeViewModel.swift
+++ Projects/Feature/HomeFeature/Sources/Home/HomeViewModel.swift
@@ -4,13 +4,17 @@ import ApplicationsDomainInterface
 
 final class HomeViewModel: BaseViewModel {
     @Published var applicationList: ApplicationListEntity?
+    @Published var studentInfo: StudentInfoEntity?
     @Published var isNavigateRecruitment: Bool = false
     @Published var isNavigateFindWorkSpace: Bool = false
 
     private let fetchApplicationUseCase: FetchApplicationUseCase
+    private let fetchStudentInfoUseCase: FetchStudentInfoUseCase
 
     public init(fetchApplicationUseCase: any FetchApplicationUseCase) {
+        fetchStudentInfoUseCase: any FetchStudentInfoUseCase
         self.fetchApplicationUseCase = fetchApplicationUseCase
+        self.fetchStudentInfoUseCase = fetchStudentInfoUseCase
     }
 
     func onAppear() {
@@ -18,6 +22,13 @@ final class HomeViewModel: BaseViewModel {
             fetchApplicationUseCase.execute()
         ) { [weak self] applicationList in
             self?.applicationList = applicationList
+        }
+    }
+    private func fetchStudentInfo() {
+        addCancellable(
+            fetchStudentInfoUseCase.execute()
+        ) { [weak self] studentInfo in
+            self?.studentInfo = studentInfo
         }
     }
 }
