3a6b18a9c96fe573405c9a2ab04abc889c7b17d4
hsj._.06 <gtw030488@gmail.com>
2023-05-24 20:29:07 +0900

    feat :: 기업찾기Component

---
+++ Projects/Feature/HomeFeature/Sources/FindWorkSpace/FindWorkSpaceComponent.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import CompaniesDomainInterface
+import NeedleFoundation
+
+public protocol FindWorkSpaceDependency: Dependency {
+    var fetchStudentCompanyListUseCase: any FetchStudentCompanyListUseCase { get }
+}
+
+public final class FindWorkSpaceComponent: Component<FindWorkSpaceDependency>, FindWorkSpaceFactory {
+    public func makeView() -> some View {
+        FindWorkSpaceView(
+            viewModel: .init(fetchStudentCompanyListUseCase: dependency.fetchStudentCompanyListUseCase)
+        )
+    }
+}
