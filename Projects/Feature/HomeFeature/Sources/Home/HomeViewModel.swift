41c5263c09451added12f2ea8e7122863327a35e
hsj._.06 <gtw030488@gmail.com>
2023-05-24 19:06:40 +0900

    feat :: HomeVM에 취업률 API 추가

--- Projects/Feature/HomeFeature/Sources/Home/HomeViewModel.swift
+++ Projects/Feature/HomeFeature/Sources/Home/HomeViewModel.swift
@@ -1,29 +1,51 @@
 import BaseFeature
 import Combine
 import ApplicationsDomainInterface
+import StudentsDomainInterface
 
 final class HomeViewModel: BaseViewModel {
     @Published var applicationList: ApplicationListEntity?
+    @Published var totalPassStudent: TotalPassStudentEntity?
     @Published var studentInfo: StudentInfoEntity?
     @Published var isNavigateRecruitment: Bool = false
     @Published var isNavigateFindWorkSpace: Bool = false
 
     private let fetchApplicationUseCase: FetchApplicationUseCase
+    private let fetchTotalPassStudentUseCase: FetchTotalPassStudentUseCase
     private let fetchStudentInfoUseCase: FetchStudentInfoUseCase
 
-    public init(fetchApplicationUseCase: any FetchApplicationUseCase) {
+    public init(
+        fetchApplicationUseCase: any FetchApplicationUseCase,
+        fetchTotalPassStudentUseCase: any FetchTotalPassStudentUseCase,
         fetchStudentInfoUseCase: any FetchStudentInfoUseCase
+    ) {
         self.fetchApplicationUseCase = fetchApplicationUseCase
+        self.fetchTotalPassStudentUseCase = fetchTotalPassStudentUseCase
         self.fetchStudentInfoUseCase = fetchStudentInfoUseCase
     }
 
     func onAppear() {
+        fetchApplication()
+        fetchStudentInfo()
+        fetchTotalPassStudent()
+    }
+
+    private func fetchApplication() {
         addCancellable(
             fetchApplicationUseCase.execute()
         ) { [weak self] applicationList in
             self?.applicationList = applicationList
         }
     }
+
+    private func fetchTotalPassStudent() {
+        addCancellable(
+            fetchTotalPassStudentUseCase.execute()
+        ) { [weak self] totalPassStudent in
+            self?.totalPassStudent = totalPassStudent
+        }
+    }
+
     private func fetchStudentInfo() {
         addCancellable(
             fetchStudentInfoUseCase.execute()
