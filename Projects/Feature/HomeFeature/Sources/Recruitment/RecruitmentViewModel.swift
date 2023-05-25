2cc4ce0c4a56a83e54ea1e52aeeac2fd7af187ec
hsj._.06 <gtw030488@gmail.com>
2023-05-25 18:01:23 +0900

    feat :: 모집의뢰서VM pagination function

--- Projects/Feature/HomeFeature/Sources/Recruitment/RecruitmentViewModel.swift
+++ Projects/Feature/HomeFeature/Sources/Recruitment/RecruitmentViewModel.swift
@@ -3,7 +3,9 @@ import RecruitmentsDomainInterface
 import Combine
 
 final class RecruitmentViewModel: BaseViewModel {
+    @Published var listPage: Int = 1
     @Published var recruitmentList: RecruitmentListEntity?
+    @Published var isNavigateRecruitmentDetail: Bool = false
 
     private let fetchRecruitmentListUseCase: FetchRecruitmentListUseCase
 
@@ -12,10 +14,24 @@ final class RecruitmentViewModel: BaseViewModel {
     }
 
     func onAppear() {
+        self.listPage = 1
         addCancellable(
-            fetchRecruitmentListUseCase.execute(page: 1, codeId: nil, company: nil)
+            fetchRecruitmentListUseCase.execute(page: listPage, codeId: nil, company: nil)
         ) { [weak self] recruitmentList in
             self?.recruitmentList = recruitmentList
+        }
+    }
+
+    func appendRecruitmentList(list: RecruitmentEntity) {
+        if self.recruitmentList?.recruitments.last == list {
+            listPage += 1
+            addCancellable(
+                fetchRecruitmentListUseCase.execute(
+                    page: listPage, codeId: nil, company: nil
+                )
+            ) { [weak self] recruitmentList in
+                self?.recruitmentList?.recruitments.append(contentsOf: recruitmentList.recruitments)
+            }
         }
     }
 }
