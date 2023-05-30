e65e27a86e40d54cf96eeb74c533acb148213ba6
hsj._.06 <gtw030488@gmail.com>
2023-05-30 10:27:33 +0900

    feat :: AreaCell

---
+++ Projects/Feature/RecruitmentFeature/Sources/Detail/Component/AreaCell.swift
@@ -0,0 +1,21 @@
+//
+//  AreaCell.swift
+//  RecruitmentFeatureInterface
+//
+//  Created by 홍승재 on 2023/05/30.
+//  Copyright © 2023 com.team.return. All rights reserved.
+//
+
+import SwiftUI
+
+struct AreaCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AreaCell_Previews: PreviewProvider {
+    static var previews: some View {
+        AreaCell()
+    }
+}
