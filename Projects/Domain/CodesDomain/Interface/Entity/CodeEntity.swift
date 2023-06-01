f46d71ff4f64c0c6babb27cfb90b70cedff8282d
hsj._.06 <gtw030488@gmail.com>
2023-06-01 11:05:51 +0900

    fix :: codeAPI 수정

--- Projects/Domain/CodesDomain/Interface/Entity/CodeEntity.swift
+++ Projects/Domain/CodesDomain/Interface/Entity/CodeEntity.swift
@@ -1,12 +1,11 @@
 import Foundation
 
-public struct CodeEntity: Equatable {
+public struct CodeEntity: Equatable, Hashable {
     public let code: Int
-    public let keyword, jobType: String
+    public let keyword: String
 
-    public init(code: Int, keyword: String, jobType: String) {
+    public init(code: Int, keyword: String) {
         self.code = code
         self.keyword = keyword
-        self.jobType = jobType
     }
 }
