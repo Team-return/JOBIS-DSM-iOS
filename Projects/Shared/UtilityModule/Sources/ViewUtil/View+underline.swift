ea4c83bd6369b806f8e2fb6ac14f04d418b9d2d2
hsj._.06 <gtw030488@gmail.com>
2023-06-15 00:40:14 +0900

    feat :: view+underline

---
+++ Projects/Shared/UtilityModule/Sources/ViewUtil/View+underline.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct UnderlineTextModifier: ViewModifier {
+    let color: Color
+
+    func body(content: Content) -> some View {
+        if #available(iOS 16.0, *) {
+            content
+                .underline(color: color)
+        } else {
+            content
+                .font(.body)
+                .overlay(
+                    Rectangle()
+                        .fill(color)
+                        .frame(height: 1)
+                        .padding(.top, 3),
+                    alignment: .bottom)
+        }
+    }
+}
+
+public extension View {
+    func underline(color: Color) -> some View {
+        self.modifier(UnderlineTextModifier(color: color))
+    }
+}
