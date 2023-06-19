5e29b8fb15899c0de0a9e3195bc7a9f97f51aa30
hsj._.06 <gtw030488@gmail.com>
2023-06-19 21:37:02 +0900

    feat :: StringUtil

---
+++ Projects/Shared/UtilityModule/Sources/StringUtil.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+public extension String {
+    func formatAsGradeClassStudent() -> String {
+        guard let number = Int(self) else {
+            return "0000"
+        }
+        
+        let grade = number / 1000
+        let classNumber = (number % 1000) / 10
+        let studentNumber = number % 100
+        
+        return "\(grade)학년 \(classNumber)반 \(studentNumber)번"
+    }
+}
