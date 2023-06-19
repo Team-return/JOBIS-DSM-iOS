5e6a711f5e5b372cd1d5c0ad1d8a1b91a4479b74
hsj._.06 <gtw030488@gmail.com>
2023-06-19 13:36:32 +0900

    feat :: ImagePicker

---
+++ Projects/Core/DesignSystem/Sources/Picker/ImagePicker.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    private let controller = UIImagePickerController()
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(photoPicker: self)
+    }
+    final class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let photoPicker: ImagePicker
+        init(photoPicker: ImagePicker) {
+            self.photoPicker = photoPicker
+        }
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+        ) {
+            if let image = info[.editedImage] as? UIImage {
+                guard let data = image.jpegData(compressionQuality: 0.1),
+                      let compressedImage = UIImage(data: data) else { return }
+                photoPicker.image = compressedImage
+            }
+            picker.dismiss(animated: true)
+        }
+    }
+    func makeUIViewController(context: Context) -> some UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.allowsEditing = true
+        return picker
+    }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    }
+}
