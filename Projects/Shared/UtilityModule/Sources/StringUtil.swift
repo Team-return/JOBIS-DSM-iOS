b9b2cd9981c206bd3492dc56c654859097819e5c
hsj._.06 <gtw030488@gmail.com>
2023-06-20 09:00:44 +0900

    feat :: File 변환 extension

--- Projects/Shared/UtilityModule/Sources/StringUtil.swift
+++ Projects/Shared/UtilityModule/Sources/StringUtil.swift
@@ -5,11 +5,11 @@ public extension String {
         guard let number = Int(self) else {
             return "0000"
         }
-        
+
         let grade = number / 1000
         let classNumber = (number % 1000) / 10
         let studentNumber = number % 100
-        
+
         return "\(grade)학년 \(classNumber)반 \(studentNumber)번"
     }
 }
