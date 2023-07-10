97851c1050ef0067934914d72eabec0352dbf5cc
hsj._.06 <gtw030488@gmail.com>
2023-07-10 22:51:41 +0900

    CI :: Xcode Cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,4 @@
-#!/bin/sh
 sh ci_install_tuist.sh
 cd ../
-git clone https://github.com/team-aliens/DMS-XCConfig.git
-mv DMS-XCConfig/XCConfig/ .
-
 ci_scripts/tuist/tuist fetch
-TUIST_CI=1 ci_scripts/tuist/tuist generate
+ci_scripts/tuist/tuist generate
