291fe883af76c69d4b4b43487c6644f3a334f42d
hsj._.06 <gtw030488@gmail.com>
2023-07-10 23:12:19 +0900

    CI :: edit CI Script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
 sh ci_install_tuist.sh
 cd ../
+git clone https://github.com/Team-return/JOBIS-XCConfig.git
+mv JOBIS-XCConfig/XCConfig/ .
+
 ci_scripts/tuist/tuist fetch
-ci_scripts/tuist/tuist generate
+TUIST_CI=1 ci_scripts/tuist/tuist generate
