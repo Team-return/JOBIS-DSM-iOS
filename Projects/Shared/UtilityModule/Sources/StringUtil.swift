a9032495bc84381528275efdc67d692145a6a547
hsj._.06 <gtw030488@gmail.com>
2023-07-11 09:48:26 +0900

    fix :: 학번이름 바꾸는 함수 수정

--- Projects/Shared/UtilityModule/Sources/StringUtil.swift
+++ Projects/Shared/UtilityModule/Sources/StringUtil.swift
@@ -2,14 +2,14 @@ import Foundation
 
 public extension String {
     func formatAsGradeClassStudent() -> String {
-        guard let number = Int(self) else {
-            return "0000"
+        guard self.count == 4 else {
+            return "학번값이 이상해요"
         }
 
-        let grade = number / 1000
-        let classNumber = (number % 1000) / 10
-        let studentNumber = number % 100
+        let grade = self.prefix(1)
+        let classroom = self.dropFirst().prefix(1)
+        let number = self.dropFirst(2)
 
-        return "\(grade)학년 \(classNumber)반 \(studentNumber)번"
+        return "\(grade)학년 \(classroom)반 \(number)번"
     }
 }
