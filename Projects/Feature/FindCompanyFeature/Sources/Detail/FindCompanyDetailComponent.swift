188727fee1d3be9d088ef98d6cac9107eb7db0d2
hsj._.06 <gtw030488@gmail.com>
2023-07-11 11:53:13 +0900

    feat :: navigation view 추가

--- Projects/Feature/FindCompanyFeature/Sources/Detail/FindCompanyDetailComponent.swift
+++ Projects/Feature/FindCompanyFeature/Sources/Detail/FindCompanyDetailComponent.swift
@@ -9,18 +9,23 @@ public protocol FindCompanyDetailDependency: Dependency {
     var fetchCompanyInfoDetailUseCase: any FetchCompanyInfoDetailUseCase { get }
     var fetchReviewListUseCase: any FetchReviewListUseCase { get }
     var recruitmentDetailFactory: any RecruitmentDetailFactory { get }
+    var reviewDetailFactory: any ReviewDetailFactory { get }
 }
 
 public final class FindCompanyDetailComponent: Component<FindCompanyDetailDependency>, FindCompanyDetailFactory {
     public func makeView(id: String, isDetail: Bool) -> some View {
-        FindCompanyDetailView(
-            viewModel: .init(
-                fetchCompanyInfoDetailUseCase: dependency.fetchCompanyInfoDetailUseCase,
-                fetchReviewListUseCase: dependency.fetchReviewListUseCase,
-                id: id
-            ),
-            recruitmentDetailFactory: dependency.recruitmentDetailFactory,
-            isDetail: isDetail
-        )
+        NavigationView {
+            FindCompanyDetailView(
+                viewModel: .init(
+                    fetchCompanyInfoDetailUseCase: dependency.fetchCompanyInfoDetailUseCase,
+                    fetchReviewListUseCase: dependency.fetchReviewListUseCase,
+                    id: id
+                ),
+                recruitmentDetailFactory: dependency.recruitmentDetailFactory,
+                reviewDetailFactory: dependency.reviewDetailFactory,
+                isDetail: isDetail
+            )
+            .navigationBarHidden(true)
+        }
     }
 }
