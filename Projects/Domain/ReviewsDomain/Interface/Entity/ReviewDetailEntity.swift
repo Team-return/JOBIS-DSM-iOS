8aa6f589a4956c6ba13a44bc95f98f91bc14f27b
hsj._.06 <gtw030488@gmail.com>
2023-08-23 15:06:44 +0900

    feat :: Review 관련된 Entity 수정

--- Projects/Domain/ReviewsDomain/Interface/Entity/ReviewDetailEntity.swift
+++ Projects/Domain/ReviewsDomain/Interface/Entity/ReviewDetailEntity.swift
@@ -1,17 +1,11 @@
 import Foundation
 
 public struct ReviewDetailEntity: Equatable {
-    public let year: Int
-    public let writer: String
     public let qnaResponses: [QnaEntity]
 
     public init(
-        year: Int,
-        writer: String,
         qnaResponses: [QnaEntity]
     ) {
-        self.year = year
-        self.writer = writer
         self.qnaResponses = qnaResponses
     }
 }
