0f7b6e6b2ac5f1e612b5bd2cdf40e71074bbaff8
hsj._.06 <gtw030488@gmail.com>
2023-08-23 15:04:57 +0900

    feat :: 후기 작성 페이지

--- Projects/Feature/PostReviewFeature/Sources/PostReviewView.swift
+++ Projects/Feature/PostReviewFeature/Sources/PostReviewView.swift
@@ -1,16 +1,104 @@
 import DesignSystem
 import SwiftUI
+import Combine
 
 struct PostReviewView: View {
+    private enum FocusField {
+        case title
+        case content
+    }
+    @FocusState private var focusField: FocusField?
     @StateObject var viewModel: PostReviewViewModel
+    @Environment(\.dismiss) var dismiss
+
+    let companyID: Int
 
     init(
-        viewModel: PostReviewViewModel
+        viewModel: PostReviewViewModel,
+        companyID: Int
     ) {
         _viewModel = StateObject(wrappedValue: viewModel)
+        self.companyID = companyID
     }
 
     var body: some View {
-        Text("Text")
+        ZStack {
+            ScrollView {
+                VStack(spacing: 0) {
+                    ForEach(0..<viewModel.qnaElements.count, id: \.self) { index in
+                        PostReviewCell(qnaElement: $viewModel.qnaElements[index])
+                    }
+
+                    Button {
+                        viewModel.qnaElements.append(.init(question: "", answer: "", codeID: 0))
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .foregroundColor(.Main.lightBlue)
+                            .frame(width: 12, height: 12)
+                            .padding(9)
+                            .background(Color.Sub.gray10)
+                            .clipShape(Circle())
+                            .shadow(blur: 4)
+                            .padding(.horizontal, 20)
+                    }
+
+                    SolidBtn(text: "완료", size: .large) {
+                        viewModel.postReview(companyID: self.companyID)
+                    }
+                }
+                .padding(.horizontal, 20)
+            }
+
+            if viewModel.isLoading {
+                Color.black.opacity(0.3).ignoresSafeArea()
+
+                ProgressView().progressViewStyle(.circular)
+            }
+        }
+        .background(Color.Sub.gray10)
+        .onAppear {
+            viewModel.onAppear()
+        }
+        .navigationTitle("버그 제보하기")
+        .navigationBarItems(trailing: majorPicker())
+        .onChange(of: viewModel.isSuccessPost) { _ in
+            dismiss()
+        }
+        .jobisToast(
+            isShowing: $viewModel.isErrorOcuured,
+            message: viewModel.errorMessage,
+            style: .error
+        )
+    }
+
+    @ViewBuilder
+    func majorPicker() -> some View {
+        Menu {
+            ForEach(viewModel.jobCodeList?.codes ?? [], id: \.self) { jobCode in
+                Button {
+                    viewModel.selectedJobCode = jobCode
+                } label: {
+                    HStack {
+                        Text(jobCode.keyword)
+
+                        if viewModel.selectedJobCode == jobCode {
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+            }
+        } label: {
+            HStack(spacing: 5) {
+                Spacer()
+
+                Text(viewModel.selectedJobCode.keyword)
+
+                JOBISIcon(.chevronDown)
+                    .frame(width: 16, height: 16)
+            }
+            .frame(width: 75)
+        }
+        .menuStyle(BorderlessButtonMenuStyle())
     }
 }
--- Projects/Feature/PostReviewFeature/Sources/PostReviewViewModel.swift
+++ Projects/Feature/PostReviewFeature/Sources/PostReviewViewModel.swift
@@ -1,5 +1,63 @@
 import BaseFeature
 import Combine
+import CodesDomainInterface
+import ReviewsDomainInterface
 
 final class PostReviewViewModel: BaseViewModel {
+    @Published var jobCodeList: CodeListEntity?
+    @Published var selectedJobCode: CodeEntity = .init(code: 0, keyword: "")
+    @Published var qnaElements: [QnaElementRequestDTO] = [
+        .init(question: "", answer: "", codeID: 0)
+    ]
+    @Published var isSuccessPost: Bool = false
+
+    private let fetchCodesUseCase: FetchCodesUseCase
+    private let postReviewUseCase: PostReviewUseCase
+
+    init(
+        fetchCodesUseCase: FetchCodesUseCase,
+        postReviewUseCase: PostReviewUseCase
+    ) {
+        self.fetchCodesUseCase = fetchCodesUseCase
+        self.postReviewUseCase = postReviewUseCase
+    }
+
+    func onAppear() {
+        fetchJobCodeList()
+    }
+
+    private func fetchJobCodeList() {
+        addCancellable(
+            fetchCodesUseCase.execute(
+                keyword: nil,
+                type: .job,
+                parentCode: nil
+            )
+        ) { [weak self] jobCodeList in
+            guard let codesFirst = jobCodeList.codes.first else { return }
+            self?.selectedJobCode = codesFirst
+            self?.jobCodeList = jobCodeList
+        }
+    }
+
+    func postReview(companyID: Int) {
+        let qnaElements = self.qnaElements.map {
+            QnaElementRequestDTO(
+                question: $0.question,
+                answer: $0.answer,
+                codeID: selectedJobCode.code
+            )
+        }
+
+        addCancellable(
+            postReviewUseCase.execute(
+                req: .init(
+                    companyID: companyID,
+                    qnaElements: qnaElements
+                )
+            )
+        ) { [weak self] _ in
+            self?.isSuccessPost = true
+        }
+    }
 }
