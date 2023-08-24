f4521aee0c711858e8134c5cf4b9d1aaebf67a5c
hsj._.06 <gtw030488@gmail.com>
2023-08-24 13:18:33 +0900

    feat :: JOBIS - URLImage

---
+++ Projects/Core/DesignSystem/Sources/Image/URLImage.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+import Kingfisher
+
+public struct URLImage: View {
+    let imageURL: URL?
+    let size: CGSize
+
+    public init(imageURL: String, size: CGFloat) {
+        let baseURL = "https://jobis-bucket.s3.ap-northeast-2.amazonaws.com/"
+        self.imageURL = URL(string: baseURL + imageURL)
+        self.size = .init(width: size, height: size)
+    }
+
+    public var body: some View {
+        KFImage(imageURL)
+            .resizable()
+            .placeholder { Circle().fill(Color.Sub.gray50) }
+            .aspectRatio(contentMode: .fit)
+            .frame(width: size.width, height: size.height)
+
+            .onAppear {
+                print(imageURL ?? "i dont know")
+            }
+    }
+}
