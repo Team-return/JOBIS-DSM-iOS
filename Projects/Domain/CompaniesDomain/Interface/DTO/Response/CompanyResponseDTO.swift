9c599f005b5548123bd5381a8f73738f49aa2adc
hsj._.06 <gtw030488@gmail.com>
2023-09-21 11:58:12 +0900

    feat :: hasRecruitment 변수 추가 (DTO)

--- Projects/Domain/CompaniesDomain/Interface/DTO/Response/CompanyResponseDTO.swift
+++ Projects/Domain/CompaniesDomain/Interface/DTO/Response/CompanyResponseDTO.swift
@@ -5,12 +5,14 @@ public struct CompanyResponseDTO: Decodable {
     public let name: String
     public let logoURL: String
     public let take: Double
+    public let hasRecruitment: Bool
 
-    public init(id: Int, name: String, logoURL: String, take: Double) {
+    public init(id: Int, name: String, logoURL: String, take: Double, hasRecruitment: Bool) {
         self.id = id
         self.name = name
         self.logoURL = logoURL
         self.take = take
+        self.hasRecruitment = hasRecruitment
     }
 
     enum CodingKeys: String, CodingKey {
@@ -18,5 +20,6 @@ public struct CompanyResponseDTO: Decodable {
         case name
         case logoURL = "logo_url"
         case take
+        case hasRecruitment = "has_recruitment"
     }
 }
