ad9f74fd5d1f8f0a8cb536d8b5d73b19cf660710
hsj._.06 <gtw030488@gmail.com>
2023-10-06 14:24:28 +0900

    feat :: update 알림

--- Projects/Feature/SplashFeature/Sources/SplashViewModel.swift
+++ Projects/Feature/SplashFeature/Sources/SplashViewModel.swift
@@ -1,10 +1,16 @@
 import BaseFeature
+import UIKit
 import Combine
 import AuthDomainInterface
 import UsersDomainInterface
 
 final class SplashViewModel: BaseViewModel {
     private let reissueTokenUseCase: any ReissueTokenUseCase
+    @Published var showUpdateAlert: Bool = false
+    @Published var showUpdateForceAlert: Bool = false
+    public var alertTitle = ""
+    public var alertMessage = ""
+    private let appleID = "6450888392"
 
     public init(
         reissueTokenUseCase: any ReissueTokenUseCase
@@ -16,14 +22,54 @@ final class SplashViewModel: BaseViewModel {
         onSuccess: @escaping (AuthorityType) -> Void,
         onError: @escaping (Error) -> Void
     ) {
-        addCancellable(reissueTokenUseCase.execute()) { authority in
-            if authority {
-                onSuccess(.developer)
-            } else {
-                onSuccess(.student)
+        update {
+            addCancellable(reissueTokenUseCase.execute()) { authority in
+                if authority {
+                    onSuccess(.developer)
+                } else {
+                    onSuccess(.student)
+                }
+            } onReceiveError: { error in
+                onError(error)
             }
-        } onReceiveError: { error in
-            onError(error)
+        }
+    }
+
+    private func update(action: () -> Void) {
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+
+        let latestVersion: String? = {
+            guard let url = URL(string: "http://itunes.apple.com/lookup?id=\(self.appleID)&country=kr"),
+                  let data = try? Data(contentsOf: url),
+                  let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
+                  let results = json["results"] as? [[String: Any]],
+                  let appStoreVersion = results[0]["version"] as? String else {
+                return nil
+            }
+            return appStoreVersion
+        }()
+
+        let marketingVersion = latestVersion!
+        let currentProjectVersion = appVersion!
+        let splitMarketingVersion = marketingVersion.split(separator: ".").map {$0}
+        let splitCurrentProjectVersion = currentProjectVersion.split(separator: ".").map {$0}
+
+        alertTitle = "업데이트 알림"
+        alertMessage = "JOBIS의 새로운 버전이 있습니다.\n\(marketingVersion) 버전으로 업데이트 해주세요."
+        if splitCurrentProjectVersion[0] < splitMarketingVersion[0] {
+            showUpdateForceAlert.toggle()
+        } else if  splitCurrentProjectVersion[1] < splitMarketingVersion[1] {
+            showUpdateForceAlert.toggle()
+        } else {
+            showUpdateAlert.toggle()
+        }
+    }
+
+    func openAppStore() {
+        let appStoreOpenUrlString = "itms-apps://itunes.apple.com/app/apple-store/\(appleID)"
+        guard let url = URL(string: appStoreOpenUrlString) else { return }
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
 }
