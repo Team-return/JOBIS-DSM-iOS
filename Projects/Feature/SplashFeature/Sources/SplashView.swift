8e5bb73a180c2dac1fcbebe7faf38f2801d870d6
hsj._.06 <gtw030488@gmail.com>
2023-10-10 23:36:30 +0900

    feat :: 강제 update 없앰

--- Projects/Feature/SplashFeature/Sources/SplashView.swift
+++ Projects/Feature/SplashFeature/Sources/SplashView.swift
@@ -18,45 +18,6 @@ struct SplashView: View {
                 .resizable()
                 .frame(width: 200, height: 200)
         }
-        .alert(isPresented: $viewModel.showUpdateAlert) {
-            if viewModel.forceAlert {
-                return Alert(
-                    title: Text(viewModel.alertTitle),
-                    message: Text(viewModel.alertMessage),
-                    dismissButton: Alert.Button.destructive(
-                        Text("확인"),
-                        action: {
-                            viewModel.openAppStore()
-                        }
-                    )
-                )
-            } else {
-                let reissue: () -> Void = {
-                    viewModel.compareRefreshToke { authority in
-                        appState.authority = authority
-                        appState.sceneFlow = .main
-                    } onError: { _ in
-                        appState.sceneFlow = .auth
-                    }
-                }
-
-                return Alert(
-                    title: Text(viewModel.alertTitle),
-                    message: Text(viewModel.alertMessage),
-                    primaryButton: Alert.Button.destructive(
-                        Text("확인"),
-                        action: {
-                            viewModel.openAppStore()
-                            reissue()
-                        }
-                    ),
-                    secondaryButton: Alert.Button.cancel(
-                        Text("취소"),
-                        action: reissue
-                    )
-                )
-            }
-        }
         .onAppear {
             viewModel.onAppear { authority in
                 appState.authority = authority
