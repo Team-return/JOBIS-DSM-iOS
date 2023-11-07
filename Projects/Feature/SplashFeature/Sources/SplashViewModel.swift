4a5f18067e0040f7560bf37fc12a608632bde3b3
hsj._.06 <gtw030488@gmail.com>
2023-11-07 22:44:50 +0900

    refactoring :: Version API 도입

--- Projects/Feature/SplashFeature/Sources/SplashViewModel.swift
+++ Projects/Feature/SplashFeature/Sources/SplashViewModel.swift
@@ -6,22 +6,26 @@ import UsersDomainInterface
 
 final class SplashViewModel: BaseViewModel {
     private let reissueTokenUseCase: any ReissueTokenUseCase
+    private let fetchMarketVersionUseCase: any FetchMarketVersionUseCase
+
     @Published var showUpdateAlert: Bool = false
     @Published var forceAlert: Bool = false
     public var alertMessage = ""
-    private let appleID = "6450888392"
 
     public init(
-        reissueTokenUseCase: any ReissueTokenUseCase
+        reissueTokenUseCase: any ReissueTokenUseCase,
+        fetchMarketVersionUseCase: any FetchMarketVersionUseCase
     ) {
         self.reissueTokenUseCase = reissueTokenUseCase
+        self.fetchMarketVersionUseCase = fetchMarketVersionUseCase
     }
 
     func onAppear(
         onSuccess: @escaping (AuthorityType) -> Void,
         onError: @escaping (Error) -> Void
     ) {
-        update {
+        print("!")
+        fetchMarketingVersion {
             self.compareRefreshToke(onSuccess: onSuccess, onError: onError)
         }
     }
@@ -41,48 +45,64 @@ final class SplashViewModel: BaseViewModel {
         }
     }
 
-    private func update(action: @escaping () -> Void) {
-        guard let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else { return }
-        let urlString = "http://itunes.apple.com/lookup?id=\(self.appleID)&country=kr"
+    private func fetchMarketingVersion(action: @escaping () -> Void) {
+        print("response")
+        addCancellable(fetchMarketVersionUseCase.execute()) { [weak self] version in
+            print("version:", version)
+            self?.update(marketingVersion: version, action: action)
+        }
+    }
 
-        guard appVersion != "0.0.0" else { return action() }
-        guard let url = URL(string: urlString)  else { return }
-        let urlRequest = URLRequest(url: url)
-        URLSession.shared.dataTask(with: urlRequest) { data, _, _ in
-            guard let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any],
-                  let results = json["results"] as? [[String: Any]],
-                  let marketingVersion = results[0]["version"] as? String else { return }
-            let currentProjectVersion = appVersion
-            let splitMarketingVersion = marketingVersion.split(separator: ".").map {$0}
-            let splitCurrentProjectVersion = currentProjectVersion.split(separator: ".").map {$0}
-            guard let intAppVersion = Int(appVersion.replacingOccurrences(of: ".", with: "")),
-                  let intMarketingVersion = Int(marketingVersion.replacingOccurrences(of: ".", with: "")),
-                  intAppVersion < intMarketingVersion else { return action() }
+    private func update(marketingVersion: String, action: @escaping () -> Void) {
+        print("update 검사")
+        guard let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
+              appVersion != "0.0.0",
+              let intAppVersion = appVersion.intAndRemoveDot(),
+              let intMarketingVersion = marketingVersion.intAndRemoveDot(),
+              intAppVersion < intMarketingVersion else { return action() }
+        print("T.T", intAppVersion, intMarketingVersion)
+        showAlert(
+            appVersionArray: appVersion.split(separator: ".").map { $0 },
+            marketingVersionArray: marketingVersion.split(separator: ".").map { $0 },
+            action: action
+        )
+    }
 
-            DispatchQueue.main.async {
-                if splitCurrentProjectVersion[0] < splitMarketingVersion[0] {
-                    self.alertMessage = "JOBIS의 새로운 버전이 있습니다.\n\(marketingVersion) 버전으로 업데이트 해주세요.\n(현재: \(appVersion))"
-                    self.showUpdateAlert.toggle()
-                    self.forceAlert.toggle()
-                } else if splitCurrentProjectVersion[1] < splitMarketingVersion[1] {
-                    self.alertMessage = "새로운 기능이 추가되었어요\n\(marketingVersion) 버전으로 업데이트 해주세요.\n(현재:  \(appVersion))"
-                    self.showUpdateAlert.toggle()
-                } else if  splitCurrentProjectVersion[2] < splitMarketingVersion[2] {
-                    self.alertMessage = "\(marketingVersion) 버전으로 업데이트를 권장합니다.\n버그가 고쳐졌을 수도 있어요.\n(현재:  \(appVersion))"
-                    self.showUpdateAlert.toggle()
-                } else {
-                    action()
-                }
+    private func showAlert(
+        appVersionArray appVersion: [String.SubSequence],
+        marketingVersionArray marketingVersion: [String.SubSequence],
+        action: @escaping () -> Void
+    ) {
+        let mVersion = marketingVersion.joined(separator: ".")
+        let aVersion = appVersion.joined(separator: ".")
+        DispatchQueue.main.async {
+            if appVersion[0] < marketingVersion[0] {
+                self.alertMessage = "JOBIS의 새로운 버전이 있습니다.\n\(mVersion) 버전으로 업데이트 해주세요.\n(현재: \(aVersion))"
+                self.showUpdateAlert.toggle()
+                self.forceAlert.toggle()
+            } else if appVersion[1] < marketingVersion[1] {
+                self.alertMessage = "새로운 기능이 추가되었어요\n\(mVersion) 버전으로 업데이트 해주세요.\n(현재: \(aVersion))"
+                self.showUpdateAlert.toggle()
+            } else if  appVersion[2] < marketingVersion[2] {
+                self.alertMessage = "\(mVersion) 버전으로 업데이트를 권장합니다.\n버그가 고쳐졌을 수도 있어요.\n(현재: \(aVersion))"
+                self.showUpdateAlert.toggle()
+            } else {
+                action()
             }
         }
-        .resume()
     }
 
     func openAppStore() {
-        let appStoreOpenUrlString = "itms-apps://itunes.apple.com/app/apple-store/\(appleID)"
+        let appStoreOpenUrlString = "itms-apps://itunes.apple.com/app/apple-store/6450888392"
         guard let url = URL(string: appStoreOpenUrlString) else { return }
         if UIApplication.shared.canOpenURL(url) {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
+    }
+}
+
+private extension String {
+    func intAndRemoveDot() -> Int? {
+        Int(self.replacingOccurrences(of: ".", with: ""))
     }
 }
