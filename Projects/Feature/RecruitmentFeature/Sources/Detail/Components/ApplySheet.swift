3b6db1085a7d9d27b497452aebd691e93fd7ac7a
HongSJae <gtw030488@gmail.com>
2023-11-29 23:12:07 +0900

    feat :: 지원하기 버튼 disable 조건 추가

--- Projects/Feature/RecruitmentFeature/Sources/Detail/Components/ApplySheet.swift
+++ Projects/Feature/RecruitmentFeature/Sources/Detail/Components/ApplySheet.swift
@@ -67,6 +67,7 @@ struct ApplySheet: View {
                     applyAction()
                     hideKeyboard()
                 }
+                .disabled(documents.isEmpty)
 
                 Spacer()
             }
